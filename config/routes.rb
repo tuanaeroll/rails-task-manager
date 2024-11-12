a53c21d972cfa64f6740384d97a83b09f55a8a56
tuana erol <tuanaerol59@gmail.com>
2024-11-12 16:39:10 +0100

    2nd is done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# config/routes.rb
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "tasks",to:"tasks#index"
+  get "tasks/new", to:"tasks#new"
+  get "tasks/:id", to:"tasks#show", as:"task"
+  post "tasks", to:"tasks#create"
+  get "tasks/:id/edit", to:"tasks#edit", as:"task_edit"
+  patch "tasks/:id", to:"tasks#update"
+  delete "tasks/:id", to:"tasks#destroy", as:"task_delete"
   # Defines the root path route ("/")
   # root "posts#index"
 end
